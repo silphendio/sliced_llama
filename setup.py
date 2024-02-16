ef3a7030752a7f507c00cd4c8219fcc0a2b9caf5
Silphendio <markus.ginthoer@gmx.at>
2024-02-16 02:37:21 +0100

    fixed installation script removed call to nvcc. The CUDA SDK isn't necessary to install CUDA apps. Use generic requirements when no compatible python version is used. Sadly, this still doesn't seem to ork on windows

--- setup.py
+++ setup.py
@@ -4,28 +4,42 @@ from subprocess import run
 import sys
 import os
 
+# create venv, try different python commands
 if not os.path.isdir(".venv"):
-    run(["python3", "-m", "venv", ".venv"])
+    print("creating virtual environment...")
+    try:
+        run(["python", "-m", "venv", ".venv"])
+    except: pass
+if not os.path.isdir(".venv"):
+    try:
+        run(["python3", "-m", "venv", ".venv"])
+    except: pass
+if not os.path.isdir(".venv"):
+    print("failed to create virtual environment.")
+
 
 requirements_file = "requirements.txt"
 
-try:
-    a = run(["nvcc", "--version"], capture_output=True)
-    if b'release 12' in a.stdout:
-        requirements_file = "requirements-cu122.txt"
-        print("found CUDA 12")
-    elif b'release 11' in a.stdout:
-        requirements_file = "requirements-cu118.txt"
-        print("found CUDA 11")
-except:
-    print("no CUDA found")
+# TODO: switch to conda to make sure that there's always a compatble python version
+if sys.version.startswith("3.11") or sys.version.startswith("3.10"):
     try:
-        run("rocminfo", capture_output=True)
-        # TODO: check version
-        print("ROCm found")
+        a = run("nvidia-smi", capture_output=True)
+        if b'CUDA Version: 12' in a.stdout:
+            requirements_file = "requirements-cu122.txt"
+            print("found CUDA 12")
+        elif b'CUDA Version: 11' in a.stdout:
+            requirements_file = "requirements-cu118.txt"
+            print("found CUDA 11")
     except:
-        print("no ROCm found")
+        print("no CUDA found")
+        try:
+            run("rocminfo", capture_output=True)
+            # TODO: check version
+            print("ROCm found")
+        except:
+            print("no ROCm found")
 
-
-run(["./.venv/bin/pip", "install", "-r", requirements_file])
-run(["echo", "install", "-r", requirements_file])
+if sys.platform == 'win32':
+    run(["./.venv/Scripts/pip", "install", "-r", requirements_file])
+else:
+    run(["./.venv/bin/pip", "install", "-r", requirements_file])
